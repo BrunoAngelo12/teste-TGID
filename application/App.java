79c28af189688cec72f2fe0e019f95df1c60cfa6
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-09-08 10:56:03 -0300

    criado a base inicial do cadastro

--- application/App.java
+++ application/App.java
@@ -7,14 +7,23 @@ public class App{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("  -  Welcome to the TGID System :)  -\n");
-        int numberEntered;
-        try{
-            System.out.print("To get started, register your CPF for an individual or CNPJ for a company: ");
-            numberEntered = sc.nextInt();
-        }catch (InputMismatchException e){
-            System.out.println("Error: You did not enter a valid number, please try again.");
-        }
-        System.out.println(numberEntered);   
+        int intNumberEntered = 0;
+        int intNumberOfDigits;
+        do{
+            try{
+                System.out.print("To get started, register your CPF for an individual or CNPJ for a company: ");
+                intNumberEntered = sc.nextInt();
+                String strNumberEnteredToString = Integer.toString(intNumberEntered);
+                intNumberOfDigits = strNumberEnteredToString.length();
+                if(intNumberOfDigits != 11 && intNumberOfDigits != 14){
+                    System.out.println(" - Enter a valid CPF or CNPJ number - ");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Error: You did not enter a valid character, please try again.");
+                break;
+            }
+        }while(intNumberEntered != 11 && intNumberEntered != 14);        
+        
         sc.close();
     }
 }
