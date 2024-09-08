5b6a3b4d8f0a6deffbff3e92b9f9e9e85531c0e7
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-09-08 12:45:05 -0300

    melhorado saida de dados para o usuário

--- application/App.java
+++ application/App.java
@@ -4,47 +4,105 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import application.entities.Client;
+import application.entities.Corporation;
 
 public class App{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("  -  Welcome to the TGID System :)  -\n");
-        userRegistration(sc);
- 
-
+        System.out.println("\n  -  Welcome to the TGID System :)  -\n");
+        System.out.println("To proceed with registration, enter the desired number:\n1 - CPF\n2 - CNPJ\n3 - Exit");
+        System.out.print("Enter a number: ");
+        byte byteSelection = sc.nextByte();
+        switch (byteSelection) {
+            case 1:
+                Client newClient = clientRegistration(sc);
+                break;
+            case 2:
+                Corporation newCorporation = corporationRegistration(sc);
+                break;
+            case 3:
+                System.out.println("Feel free! :)");
+                break;
+            default:
+                System.out.println("Select a valid number");
+                break;
+        }
         sc.close();
     }
 
-    public static void userRegistration(Scanner sc){
+    public static Client clientRegistration(Scanner sc){
         long longNumberEntered = 0;
         long longNumberOfDigits = 0;
         boolean booNumberOk = false;
+        boolean booApplicationError = false;
             do{
                 try{
-                    System.out.print("To get started, register your CPF for an individual or CNPJ for a company: ");
+                    System.out.print("To get started, register your CPF (11 Digits): ");
                     longNumberEntered = sc.nextLong();
 
 
                     String strNumberEnteredToString = Long.toString(longNumberEntered);
                     longNumberOfDigits = strNumberEnteredToString.length();
 
-                    if(longNumberOfDigits == 11 || longNumberOfDigits == 14){
+                    if(longNumberOfDigits == 11){
                         booNumberOk = true;
                     }else{
-                        System.out.println("- Enter a valid CPF or CNPJ number - ");
+                        System.out.println("- Enter a valid CPF - ");
                     }
                 }catch (InputMismatchException e){
                     System.out.println("Error: You did not enter a valid character, please try again.");
+                    booApplicationError = true;
+                    break;
+                }
+            }while(booNumberOk == false);
+            
+            if(booApplicationError == true){
+                System.out.print("");
+            }else{
+                String strPassword = passwordRegistration(sc);
+                Client newClient = new Client(longNumberEntered, strPassword);
+                System.out.println("Customer registered successfully!");
+                return newClient;
+            }
+            return null;
+    }
+
+    public static Corporation corporationRegistration(Scanner sc){
+        long longNumberEntered = 0;
+        long longNumberOfDigits = 0;
+        boolean booNumberOk = false;
+        boolean booApplicationError = false;
+            do{
+                try{
+                    System.out.print("To get started, register your CNPJ (14 Digits): ");
+                    longNumberEntered = sc.nextLong();
+
+
+                    String strNumberEnteredToString = Long.toString(longNumberEntered);
+                    longNumberOfDigits = strNumberEnteredToString.length();
+
+                    if(longNumberOfDigits == 14){
+                        booNumberOk = true;
+                    }else{
+                        System.out.println("- Enter a valid CNPJ - ");
+                    }
+                }catch (InputMismatchException e){
+                    System.out.println("Error: You did not enter a valid character, please try again.");
+                    booApplicationError = true;
                     break;
                 }
             }while(booNumberOk == false);
 
-            String strPassword = passwordRegistration(sc);
-            if(longNumberOfDigits == 11){
-                Client newClient = new Client(longNumberEntered, strPassword);
-            }
-            
+            if(booApplicationError == true){
+                System.out.print("");
+            }else{
+                String strPassword = passwordRegistration(sc);
+                Corporation newCorporation = new Corporation(longNumberEntered, strPassword);
+                System.out.println("Company registered successfully!");
+                return newCorporation;
+            }    
+            return null;
     }
 
     public static String passwordRegistration(Scanner sc){
