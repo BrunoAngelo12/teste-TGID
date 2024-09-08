4e95cdf175d13abefc66bb0a27b55a3c4d256c9d
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-09-08 11:47:54 -0300

    criado o metodo para registro de cpf ou cnpj

--- application/App.java
+++ application/App.java
@@ -6,24 +6,36 @@ import java.util.Scanner;
 public class App{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("  -  Welcome to the TGID System :)  -\n");
-        int intNumberEntered = 0;
-        int intNumberOfDigits;
-        do{
-            try{
-                System.out.print("To get started, register your CPF for an individual or CNPJ for a company: ");
-                intNumberEntered = sc.nextInt();
-                String strNumberEnteredToString = Integer.toString(intNumberEntered);
-                intNumberOfDigits = strNumberEnteredToString.length();
-                if(intNumberOfDigits != 11 && intNumberOfDigits != 14){
-                    System.out.println(" - Enter a valid CPF or CNPJ number - ");
-                }
-            }catch (InputMismatchException e){
-                System.out.println("Error: You did not enter a valid character, please try again.");
-                break;
-            }
-        }while(intNumberEntered != 11 && intNumberEntered != 14);        
-        
+        register(sc);
+        System.out.println(":)");  
+
         sc.close();
+    }
+
+    public static void register(Scanner sc){
+        long longNumberEntered = 0;
+        long longNumberOfDigits;
+        boolean booNumberOk = false;
+            do{
+                try{
+                    System.out.print("To get started, register your CPF for an individual or CNPJ for a company: ");
+                    longNumberEntered = sc.nextLong();
+
+
+                    String strNumberEnteredToString = Long.toString(longNumberEntered);
+                    longNumberOfDigits = strNumberEnteredToString.length();
+
+                    if(longNumberOfDigits == 11 || longNumberOfDigits == 14){
+                        booNumberOk = true;
+                    }else{
+                        System.out.println("- Enter a valid CPF or CNPJ number - ");
+                    }
+                }catch (InputMismatchException e){
+                    System.out.println("Error: You did not enter a valid character, please try again.");
+                    break;
+                }
+            }while(booNumberOk == false);        
     }
 }
